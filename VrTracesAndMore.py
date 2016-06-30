4deb34ff7d189973e9de44d89937d911f95a562d
ElPolloFrio <ElPolloFrio@users.noreply.github.com>
2016-06-30 08:49:40 -0500

    Switch from asking users to edit the script directly to asking users to edit an external config file. Provide a sample config file and sample Vr data set.

--- VrTracesAndMore.py
+++ VrTracesAndMore.py
@@ -11,7 +11,7 @@
 # See the README file for details.
 
 import numpy as np
-import string, logging
+import string, logging, os
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
@@ -19,6 +19,8 @@ import matplotlib.font_manager
 import matplotlib.tri as tri
 
 def setUpTheLogger():
+    # Someone set up us the logger? Ha ha ha.
+
     # Create the logger object.
     logger = logging.getLogger('vrtraces')
     logger.setLevel(logging.INFO)
@@ -51,3 +53,124 @@ def load_file(filename):
     return data
 
 
+def setVars(configfile):
+    # Define some variables in an external configuration file to make this
+    # script easier to use for people who don't program...which is,
+    # unfortunately, the expected user base.
+
+    # filename: (string) Filename containing mesovortex data points. Either
+    #   specify the full path (e.g.,
+    #   C:\\Users\\LMK\\Documents\\Python code\\sampleVR.csv on Windows) or
+    #   the filename alone if the CSV file is in the same directory as this
+    #   script.
+    #
+    # mv_name: (string) Name or designation of the mesovortex. This is used
+    #   in the figure titles. 
+    #
+    # vr_ylim: (integer or decimal) Upper limit of the y-axis for the Vr
+    #   plots. Pick a value that's near or slightly higher than the highest
+    #   height in the spreadsheet for Vr values.
+    #
+    # tz_units: (string) Time zone designation to use on the x-axis for all
+    #   time series plots. This should be the same time zone used in the
+    #   spreadsheet. Typically, that would be UTC since radar data sets are
+    #   in UTC. However, some forecasters might prefer to keep their
+    #   spreadsheets in local time instead.
+    #
+    # corediam_units: (string) Core diameter units to use on the y-axis for
+    #   core diameter plots. 
+    #
+    # hght_units: (string) Height units to use on the y-axis for the Vr plots.
+    #
+    # corediam_colmap: (string, must be a valid matplotlib colormap name)
+    #   A matplotlib colormap from which to select colors to use for the core
+    #   diameter plot. Good options include 'hsv', 'gist_rainbow', 'Spectral',
+    #   and 'Paired'. Note that these names are case-sensitive.
+    #
+    # filled_contour_colmap: (string, must be a valid matplotlib colormap name)
+    #   A matplotlib colormap to use on the filled contour plots. Good options
+    #   include 'hot_r'.
+    #
+    # modulo_for_timeticks: (integer) Increase this value to 2 or 4 if too
+    #   many time ticks are shown on the x-axis. To show all time ticks, set
+    #   this value to 1.
+    #
+    # VrPointsBins: (comma-separated list of integers or decimal numbers) Bin values
+    #   for the Vr plot with color-coded Vr values.
+    #
+    # VrPointsCols: (comma-separated list of strings which must be valid matplotlib
+    #   color designations) Colors associated with the bin values defined
+    #   above. This is used on the Vr plot with color-voded Vr values.
+    #
+    # VrPointsBins_algfail_adj: (decimal) The contoured plot with
+    #   raw numbers always has problems because the data set is too sparse.
+    #   The contours for the gridded interpolation sometimes look incorrect
+    #   because of the interpolation. Sometimes, the contours look better
+    #   by contouring at 14.9 instead of 15.0, for example. Set this value
+    #   to -0.1 (use other values at your own risk) to produce a plot
+    #   with contours at VrPointsBins + VrPointsBins_algfail_adj. It
+    #   may or may not improve the appearance of the contours with the
+    #   gridded interpolation.
+    #
+    # tri_subdivisions: (integer) Another interpolation method is the
+    #   triangular interpolation. This plot is produced by default. Depending
+    #   on the data set, the contours might look better after using a
+    #   triangular refiner. Set this to the number of subdivisions for each
+    #   triangle. Typical values are 1-3. This script will disallow anything
+    #   higher than 5.
+    # 
+    # VrContoursBins: (comma-separated list of integers or decimal numbers) Bin
+    #   values for the gridded and triangulated contoured Vr plots.
+    #
+    # VrContoursCols: (comma-separated list of strings which must be valid matplotlib
+    #   color designations) Colors associated with the bin values defined above. This
+    #   is used on the gridded and triangulated contoured Vr plots.
+    #
+    # ContourIntervals: (comma-separated list of integers or decimal numbers) Bin
+    #   values for the gridded and triangulated contoured Vr plots.
+    #
+    # ContourCols: (comma-separated list of strings which must be valid matplotlib
+    #   color designations) Colors associated with the bin values defined above. This
+    #   is used on the gridded and triangulated contoured Vr plots.
+    #
+    # ContourTickLabels: (comma-separated list of integers or decimals) This is for
+    #   convenience when using the gridded contoured plots with a contouring algorithm
+    #   failure. Plotting at 14.9 but labeling it as 15, for example, because the contouring
+    #   algorithm failed when plotting at 15. Make sure that each defined interval has an
+    #   associated label.
+    #
+    # rcParamsFontSize: (integer) Ron always wanted bigger numbers. Change this to change
+    #   the font size, as in plt.rcParams['font.size'] = 18.
+    
+    dictPlotParms = {}
+
+    cfile = open(configfile, 'r')
+    clines = cfile.readlines()
+    cfile.close()
+
+    for line in clines:
+        # Drop newlines
+        line = line.strip()
+        # Ignore comment lines
+        if line[0] is not '#':
+            # Chose to use "=" as a separator b/c of colons in file paths
+            assert line.count('=') is 1, 'Oops. Only one "=" is allowed per line. Go fix this line in the config file:\n%s' % (line)
+            parts = line.split('=')
+            key = parts[0].strip()
+            val = parts[1].strip()
+            # Convert from strings to other data types
+            if key in ['vr_ylim', 'VrPoints_algfail_adjustment']:
+                val = float(val)
+            if key in ['modulo_for_timeticks', 'tri_subdivisions']:
+                val = int(val)
+            if key in ['VrPointsBins', 'VrPointsCols', 'VrContoursBins', 'VrContoursCols', 'ContourIntervals', 'ContourCols']:
+                # Turn a comma-separated text list into a Python list
+                val = val.split(',')
+                val = [x.strip() for x in val]
+                if key in ['VrPointsBins', 'VrContoursBins', 'ContourIntervals']:
+                    # Convert bin values from strings to floats
+                    val = [float(x) for x in val]
+            # Assign configuration items to the dictionary
+            dictPlotParms[key] = val
+
+    return dictPlotParms
